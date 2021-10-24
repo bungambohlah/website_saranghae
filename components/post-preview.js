e4e12185527e61a33eca3757faf91c990c4a1afb
afif <afif@garasilabs.com>
2021-10-24 09:33:30 +0700

    initial commit, create nextjs app, build strapi

--- components/post-preview.js
+++ components/post-preview.js
@@ -1,7 +1,7 @@
-import Avatar from './avatar'
-import Date from './date'
-import CoverImage from './cover-image'
-import Link from 'next/link'
+import Avatar from './avatar';
+import Date from './date';
+import CoverImage from './cover-image';
+import Link from 'next/link';
 
 export default function PostPreview({
   title,
@@ -13,19 +13,19 @@ export default function PostPreview({
 }) {
   return (
     <div>
-      <div className="mb-5">
+      <div className='mb-5'>
         <CoverImage slug={slug} title={title} url={coverImage.url} />
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
+      <h3 className='text-3xl mb-3 leading-snug'>
         <Link href={`/posts/${slug}`}>
-          <a className="hover:underline">{title}</a>
+          <a className='hover:underline'>{title}</a>
         </Link>
       </h3>
-      <div className="text-lg mb-4">
+      <div className='text-lg mb-4'>
         <Date dateString={date} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
+      <p className='text-lg leading-relaxed mb-4'>{excerpt}</p>
+      <Avatar name={author?.name} picture={author?.picture} />
     </div>
-  )
+  );
 }
